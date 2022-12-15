c265220c382bc2ccdd929e4b00d1619713fa3582
hongchen <hongchen@163.com>
2022-12-15 13:07:24 +0900

    123

--- src/main/java/com/org/Handler/AuthenticationFailure.java
+++ src/main/java/com/org/Handler/AuthenticationFailure.java
@@ -4,13 +4,10 @@ import com.google.gson.Gson;
 import com.org.Message.Msg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -22,7 +19,7 @@ public class AuthenticationFailure implements AuthenticationFailureHandler{
     Gson gson;
 
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException {
         Msg msg=null;
         if(e instanceof UsernameNotFoundException){
             msg=Msg.fail(e.getMessage());
