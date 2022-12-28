7c7aac2979426301ec5051093b09d3403fbe2f17
hongchen <hongchen@163.com>
2022-12-28 16:01:43 +0900

    123

--- src/main/java/com/org/Handler/SelfAuthenticationProvider.java
+++ src/main/java/com/org/Handler/SelfAuthenticationProvider.java
@@ -20,8 +20,8 @@ public class SelfAuthenticationProvider implements AuthenticationProvider{
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String account= authentication.getName();     //获取session用户名
-        String password= (String) authentication.getCredentials();  //获取session密码
+        String account= authentication.getName();     //获取用户名
+        String password= (String) authentication.getCredentials();  //获取密码
         UserDetails userDetails= userService.loadUserByUsername(account);  //数据库查询密码
         boolean checkPassword= bCryptPasswordEncoder.matches(password,userDetails.getPassword());  //比对session和数据库的密码
         if(!checkPassword){
